4ecbeb668fb395c2bad5ad91e31b18a92e2d90cb
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-08-12 15:54:45 +0300

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let answer = "";
+    let counter = string.length;
+    while (counter != 0) {
+        answer += string[counter - 1];
+        counter -= 1;
+    }
+    
+    return answer;
 };
 
 // Do not edit below this line
