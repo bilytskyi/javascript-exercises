55fb10b243419e9d0d1b3c6b9c113c0b1eb69309
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-08-12 16:43:22 +0300

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let answer = 0;
+    if (a > 0 && b > 0 && typeof a == typeof 5 && typeof b == typeof 5) {
+        if (a < b) {
+            for (i = a; i <= b; i++) {
+                answer += i;
+            }
+        }
+        else if (b < a) {
+            for (i = b; i <= a; i++) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
