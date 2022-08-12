f246b95e72412a601d2b536d444aa280846a33c1
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-08-12 15:41:11 +0300

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let answer = string;
+    if (num < 0) {
+        return "ERROR";
+    }
+    else if (num == 0) {
+        return "";
+    }
+    else {
+        for (i = 1; i < num; i++) {
+            answer += string;
+        }
+        return answer;
+    }
 };
 
 // Do not edit below this line
