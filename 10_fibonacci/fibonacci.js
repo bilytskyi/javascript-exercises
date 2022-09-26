c763447420fd07fec70aa3b00cd5f756b5bc2927
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-09-26 14:09:04 +0200

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return 'OOPS';
+    }
+    arr = [1, 1];
+    if (n == 1 || n == 2) {
+        return arr[n - 1];
+    }
+    if (n > 2) {
+        let counter = 2;
+        while (arr.length != n) {
+            arr[counter] = arr[counter - 1] + arr[counter - 2];
+            counter++;
+        }
+        return arr[n - 1];
+    }
 };
 
 // Do not edit below this line
