6a92411ad02a06b29b400e167eae75fa6f989ae3
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-09-26 13:54:29 +0200

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.toLowerCase();
+    let reverseText = text.split("").filter(word => word != ' ' && word != ',' && word != '!' && word != '.').reverse().join("");
+    text = text.split("").filter(word => word != ' ' && word != ',' && word != '!' && word != '.').join("");
+    console.log(reverseText);
+    console.log(text);
+    if (text == reverseText) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
