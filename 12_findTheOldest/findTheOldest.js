4a0fd17210b66bf2e52abfae038b4bc3351688ce
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-09-26 16:08:47 +0200

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const yearNow = new Date().getFullYear();
+    const oldest = people.sort(function(a, b) {
+        if (!a.hasOwnProperty('yearOfDeath')) {
+            a.yearOfDeath = yearNow;
+        }
+        if (!b.hasOwnProperty('yearOfDeath')) {
+            b.yearOfDeath = yearNow;
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+      });
+    return oldest[0];
 };
 
 // Do not edit below this line
